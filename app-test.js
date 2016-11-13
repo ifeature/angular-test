63206ccc1ca184a863e59e1817a8289ba1b3440c
ifeature <ifeature@yandex.ru>
2016-11-13 22:36:35 +0200

    :star2:

---
+++ app-test.js
@@ -0,0 +1,95 @@
+describe('myApp', () => {
+  beforeEach(module('myApp'));
+
+  describe('service', () => {
+    it('should do sum', inject(function(Calc) {
+      expect(Calc.sum(3, 4)).toBe(7);
+    }));
+  });
+
+  describe('service with $http', () => {
+    let UserService, $httpBackend, mockUsers = [{name: 'Bob'}];
+
+    beforeEach(inject(function(_UserService_, _$httpBackend_) {
+      UserService = _UserService_;
+      $httpBackend = _$httpBackend_;
+      $httpBackend.whenGET('/users').respond(mockUsers);
+      $httpBackend.whenGET('/users/1').respond(mockUsers[0]);
+    }));
+
+    it('should get one user', function(done) {
+      UserService.getOne(1).then(function(user) {
+        expect(user).toEqual(mockUsers[0]);
+        done();
+      });
+
+      $httpBackend.flush();
+    });
+
+  });
+
+  describe('controller', () => {
+    let controller, Calc;
+
+    beforeEach(inject(function(_Calc_, $controller) {
+      Calc = _Calc_;
+      spyOn(Calc, 'sum').and.returnValue(7);
+      controller = $controller('MainController', {Calc});
+    }));
+
+    it('should call Calc service', function() {
+      controller.doCalculations(3, 4);
+      expect(Calc.sum).toHaveBeenCalled();
+    });
+    it('should output results', function() {
+      controller.doCalculations(3, 4);
+      expect(controller.result).toBe(7);
+    });
+  });
+
+  describe('directive', () => {
+    let UserService, $scope, isolateScope, element, mockUsers = [{name: 'Bob'}];;
+
+    beforeEach(inject(function($q, $rootScope, $compile, _UserService_) {
+        const deffered = $q.defer();
+        UserService = _UserService_;
+        $scope = $rootScope.$new();
+        $scope.user = {name: 'Bob'};
+        element = angular.element('<user-card user="user"></user-card>');
+        $compile(element)($scope);
+        isolateScope = element.isolateScope();
+        spyOn(UserService, 'getOne').and.returnValue(deffered.promise);
+        deffered.resolve(mockUsers[0]);
+        $scope.$digest();
+    }));
+
+    it('someMethod should call UserService', function() {
+      isolateScope.someMethod();
+      expect(UserService.getOne).toHaveBeenCalled();
+    });
+
+    it('someMethod should output user', function() {
+      isolateScope.someMethod();
+      expect(isolateScope.user).toEqual({name: 'Bob'});
+    });
+  });
+
+  describe('component', () => {
+    let $componentController, $scope, controller;
+    beforeEach(inject(function($rootScope, _$componentController_) {
+      $componentController = _$componentController_;
+      $scope = $rootScope.$new();
+      controller = $componentController('helloWorld', {$scope: $scope});
+    }));
+
+    it('should be attached to the scope', function() {
+      expect($scope.$ctrl).toBe(controller);
+    });
+
+    it('should expose my title', function() {
+      expect(controller.myTitle).toBeDefined();
+      expect(controller.myTitle).toBe('Hi');
+    });
+  });
+
+});
---
+++ app.js
@@ -0,0 +1,57 @@
+angular.module('myApp', [])
+
+.service('Calc', function() {
+  this.sum = function(a, b) {
+    return a + b;
+  };
+})
+
+.service('UserService', function($http) {
+  this.getOne = function (id) {
+    return $http.get('/users/' + id).then(function(response) {
+      return response.data;
+    });
+  };
+
+  this.getAll = function () {
+    return $http.get('/users').then(function(response) {
+      return response.data;
+    });
+  };
+})
+
+.controller('MainController', function(Calc) {
+  this.doCalculations = function (a, b) {
+    this.result = Calc.sum(a, b);
+  };
+})
+
+.directive('userCard', function(UserService) {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      user: '='
+    },
+    template: '<div>{{user.name}}</div>',
+    link: function(scope) {
+      scope.someMethod = function() {
+        UserService.getOne(1).then(function(user) {
+          scope.user = user;
+        });
+      };
+    }
+  }
+})
+
+.component('helloWorld', function() {
+  return {
+    controller: function() {
+      this.myTitle = 'hi';
+
+      this.sayHi = function() {
+        this.title = 'Hello';
+      };
+    }
+  };
+});
